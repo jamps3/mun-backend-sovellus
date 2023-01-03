679a413937470b3a38df65daac5fa18b7b6a8aae
Jan-Erik Labbas <jamps3@gmail.com>
2023-01-03 11:43:37 +0200

    Randomize url implemented

--- index.js
+++ index.js
@@ -6,6 +6,36 @@ const port = process.env.PORT || 3000;
 app.use(express.static("public"));
 
 app.listen(port, () => {
-  console.log("Example app listening at http://localhost:" + port);
-  console.log("Press Ctrl+C to quit.");
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+app.get("/randomize", (req, res) => {
+  let a = Math.round(Math.random() * 2 + 1);
+  let b = Math.round(Math.random() * 2 + 1);
+  let c = Math.round(Math.random() * 2 + 1);
+  let success = false;
+  if (a == b) {
+    if (b == c) {
+      success = true;
+    }
+  }
+
+  if (success) {
+    res.send(
+      "number1: " +
+        a +
+        "<br>" +
+        "number2: " +
+        b +
+        "<br>" +
+        "number3: " +
+        c +
+        "<br>" +
+        "Success!"
+    );
+  } else {
+    res.send(
+      "number1: " + a + "<br>" + "number2: " + b + "<br>" + "number3: " + c
+    );
+  }
 });
